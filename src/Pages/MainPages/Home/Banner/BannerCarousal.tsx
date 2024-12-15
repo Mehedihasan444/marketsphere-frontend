d5313716b6995c3ee3077ee39a124c32dabb57a5
Mehedi <mehedihasan67705251@gmail.com>
2024-12-15 16:07:00 +0600

    Enhance BannerCarousel component: add rounded corners to carousel items and images for improved aesthetics.

--- src/Pages/MainPages/Home/Banner/BannerCarousal.tsx
+++ src/Pages/MainPages/Home/Banner/BannerCarousal.tsx
@@ -2,33 +2,33 @@ import React from "react";
 import { Carousel } from "antd";
 
 const BannerCarousal: React.FC = () => (
-  <Carousel autoplay arrows infinite={true} className="h-[362px]">
-    <div className="h-[362px]">
+  <Carousel autoplay arrows infinite={true} className="h-[362px] rounded-md ">
+    <div className="h-[362px] rounded-md">
       <img
         src="https://i.ibb.co.com/vxd9HQh/1212-e1733902486349.jpg"
         alt=""
-        className="object-cover h-full w-full "
+        className="object-cover h-full w-full rounded-md "
       />
     </div>
-    <div className="h-[362px]">
+    <div className="h-[362px] rounded-md">
       <img
         src="https://i.ibb.co.com/37x09cT/macbookprom3.jpg"
         alt=""
-        className="object-cover h-full w-full"
+        className="object-cover h-full w-full rounded-md"
       />
     </div>
-    <div className="h-[362px]">
+    <div className="h-[362px] rounded-md">
       <img
         src="https://i.ibb.co.com/dW4Y3HJ/Iphone-16-searise-1.jpg"
         alt=""
-        className="object-cover h-full w-full"
+        className="object-cover h-full w-full rounded-md"
       />
     </div>
-    <div className="h-[362px]">
+    <div className="h-[362px] rounded-md">
       <img
         src="https://img.freepik.com/premium-psd/smartphone-sale-banner-template_185005-374.jpg?semt=ais_hybrid"
         alt=""
-        className="object-cover h-full w-full"
+        className="object-cover h-full w-full rounded-md"
       />
     </div>
   </Carousel>
