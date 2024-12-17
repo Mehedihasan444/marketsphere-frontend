44ffffd5333ebb41b163860a5915fd66ddd3cf5f
Mehedi <mehedihasan67705251@gmail.com>
2024-12-17 16:48:44 +0600

    Add coupons section to ProductDetails: fetch and display available coupons with copy functionality

--- src/Pages/MainPages/ProductDetails/ProductDetails.tsx
+++ src/Pages/MainPages/ProductDetails/ProductDetails.tsx
@@ -5,21 +5,25 @@ import ProductInfo from "./ProductInfo/ProductInfo";
 import { useNavigate, useParams } from "react-router-dom";
 import { useGetProductByIdQuery, useGetProductsQuery } from "../../../Redux/Features/Product/productApi";
 import ProductDetailsTabs from "./Product_Details_Tabs/Product_Details_Tabs";
-import { Alert, Button, Divider, Spin } from "antd";
+import { Alert, Button, Divider, message, Spin } from "antd";
 import ProductCard from "../../../Components/Shared/ProductCard";
-import { TProduct } from "../../../Interface";
+import {  TCouponItem, TProduct } from "../../../Interface";
+import { useGetSingleShopCouponsQuery } from "../../../Redux/Features/Coupon/couponApi";
 
 const ProductDetails = () => {
   const productId = useParams<{ id: string }>().id;
   const navigate = useNavigate();
   const { data = {}, isLoading, error } = useGetProductByIdQuery(productId as string, { skip: !productId });
   const product = data?.data || {};
-
   const category = product?.category?.name;
   const { data: allProducts = {}, } = useGetProductsQuery({ category, }, { skip: !category });
   const { data: relatedProducts = [], } = allProducts?.data || {};
 
+  const { data: couponsData = {} } = useGetSingleShopCouponsQuery(product?.shop?.id, { skip: !product?.shop?.id })
+  const { couponItem: coupons = [] } = couponsData?.data || {}
 
+
+  // breadcrumbs
   const breadcrumbItems = [
     {
       href: "/",
@@ -48,12 +52,13 @@ const ProductDetails = () => {
     },
   ];
 
-
+  // extract images
   const images = product?.images?.map((image: string) => ({
     original: image,
     thumbnail: image,
     description: product.name,
   })) || [];
+
 
   if (isLoading) {
     return (
@@ -91,6 +96,59 @@ const ProductDetails = () => {
           {/* Right Section: Product Details */}
           <div className="flex-1">
             <ProductInfo product={product} />
+          </div>
+        </div>
+        <Divider />
+        {/* Coupons Section */}
+        <div className="mt-4 p-4 bg-gray-50 rounded-lg ">
+          <h3 className="text-lg font-semibold mb-4">Available Coupons</h3>
+          <div className="flex items-center gap-4 w-full overflow-x-auto shadow-inner p-2 rounded-md bg-neutral-100">
+            {coupons && coupons?.length > 0 ? (
+              coupons?.map((coupon: TCouponItem) => (
+                <div
+                  key={coupon.id}
+                  className="flex justify-between items-center border p-3 rounded-md bg-white shadow-sm hover:shadow-md transition
+                    min-w-[400px] max-w-[400px] w-full sm:w-auto "
+                >
+                  <div>
+                    <p className="text-md font-medium">
+                      Code: <span className="font-bold text-blue-600">{coupon.code}</span>
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      Discount: {coupon.discount}% off
+                    </p>
+                    <p className="text-xs text-gray-400">
+                      Expires on: {new Date(coupon.expiryDate).toLocaleDateString()}
+                    </p>
+                    <details>
+                      <summary className="text-xs text-gray-400 cursor-pointer">Details</summary>
+                      <p className="text-xs text-gray-400">{coupon.code.split("-")[1] === "100" ? "Minimum spend $500 To get discount" :
+                        coupon.code.split("-")[1] === "500" ? "Minimum spend $1000 To get discount" :
+                          coupon.code.split("-")[1] === "1000" ? "Minimum spend $1500 To get discount" :
+                            coupon.code.split("-")[1] === "1500" ? "Minimum spend $2000 To get discount" :
+                              coupon.code.split("-")[1] === "2000" ? "Minimum spend $2500 To get discount" :
+                                coupon.code.split("-")[1] === "2500" ? "Minimum spend $3000 To get discount" : "Minimum spend $5000 To get discount"
+                      }</p>
+                    </details>
+                  </div>
+                  <div>
+                    <Button
+                      type="default"
+                      variant="outlined"
+                      // className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition"
+                      onClick={() => {
+                        navigator.clipboard.writeText(coupon.code);
+                        message.success('Coupon code copied!');
+                      }}
+                    >
+                      Copy Code
+                    </Button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-500">No coupons available at the moment.</p>
+            )}
           </div>
         </div>
 
