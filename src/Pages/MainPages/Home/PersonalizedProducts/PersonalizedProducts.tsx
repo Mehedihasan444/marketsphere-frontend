b4730940b78b757bf4f11965160c3f205f84b353
Mehedi <mehedihasan67705251@gmail.com>
2025-10-15 02:21:43 +0600

    feat: enhance PersonalizedProducts component with improved header and layout adjustments

--- src/Pages/MainPages/Home/PersonalizedProducts/PersonalizedProducts.tsx
+++ src/Pages/MainPages/Home/PersonalizedProducts/PersonalizedProducts.tsx
@@ -1,4 +1,3 @@
-
 import { Alert, Skeleton, Spin } from "antd";
 import ProductCard from "../../../../Components/Shared/ProductCard";
 import { TProduct } from "../../../../Interface";
@@ -79,12 +78,12 @@ const PersonalizedProducts = () => {
     <div className=" lg:mx-16 p-4 mt-4  rounded-xl">
       {/* Header */}
       <div className="py-4">
-        <h2 className="text-xl font-semibold pb-2">Just For You</h2>
-        <hr />
+         <h2 className="text-3xl font-bold text-gray-800 pb-4">Just For You</h2>
+        <a href="/products" className="text-gray-500 hover:text-blue-700 text-sm">see all →</a>
       </div>
 
       {/* Product Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-2 items-center">
+      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-2 items-center">
         {products?.map((product: TProduct, index: number) => (
           <ProductCard product={product} key={index} />
         ))}
@@ -104,48 +103,3 @@ const PersonalizedProducts = () => {
 };
 
 export default PersonalizedProducts;
-// import { Alert, Spin } from "antd";
-// import ProductCard from "../../../../Components/Shared/ProductCard";
-// import { TProduct } from "../../../../Interface";
-// import { useGetPriorityBasedProductsQuery,  } from "../../../../Redux/Features/Product/productApi";
-
-// const PersonalizedProducts = () => {
-//   const { data = {}, isLoading, error } = useGetPriorityBasedProductsQuery("");
-//   const  {data:products} = data?.data || {};
-
-//   if (isLoading) {
-//     return (
-//       <div className="flex justify-center items-center h-screen w-full">
-//         <Spin tip="Loading..." />
-//       </div>
-//     );
-//   }
-
-//   if (error) {
-//     return (
-//       <Alert
-//         message="Error"
-//         description="Failed to load users."
-//         type="error"
-//         showIcon
-//       />
-//     );
-//   }
-//   return (
-//     <div className="bg-white lg:mx-16 p-4 mt-4">
-//         {/* filter */}
-//       <div className="py-4">
-//         <h2 className="text-xl font-semibold pb-2">Just For You</h2>
-//         <hr />
-//         </div>
-//       <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-2 items-center">
-//         {/* product list */}
-//         {products?.map((product:TProduct, index:number) => (
-//           <ProductCard product={product} key={index} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PersonalizedProducts;
