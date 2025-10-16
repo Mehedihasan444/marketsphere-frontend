84504a3ed09b3657062486a9159b7e76bcdd9934
Mehedi <mehedihasan67705251@gmail.com>
2025-10-16 15:55:15 +0600

    feat: update AllProducts component for improved product grid layout and type annotation

--- src/Pages/MainPages/Shop/AllProducts/AllProducts.tsx
+++ src/Pages/MainPages/Shop/AllProducts/AllProducts.tsx
@@ -1,14 +1,15 @@
 import ProductCard from "../../../../Components/Shared/ProductCard";
 import { TProduct } from "../../../../Interface";
 
-const AllProducts = ({ products }:{products:TProduct[]}) => {
+const AllProducts = ({ products }: { products: TProduct[] }) => {
+  
   return (
     <div className="bg-white mt-4 p-4">
       <div className="py-4">
         <h2 className="text-xl font-semibold pb-2">All Products </h2>
         <hr />
       </div>
-      <div className=" grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-6">
+      <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {products?.map((product, idx) => (
           <ProductCard key={idx} product={product} />
         ))}
